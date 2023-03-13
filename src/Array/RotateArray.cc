de35262c2bc8a91f3de68792b43b54fd8b50c3bc
Timur <timkonovalov9858@gmail.com>
2023-03-13 18:38:37 +0300

    RotateArray

---
+++ src/Array/RotateArray.cc
@@ -0,0 +1,11 @@
+#include "vector"
+
+class Solution {
+ public:
+  void rotate(std::vector<int> &nums, int k) {
+    k = k % nums.size();
+    reverse(nums.begin(), nums.end());
+    reverse(nums.begin(), nums.begin() + k);
+    reverse(nums.begin() + k , nums.end());
+  }
+};
