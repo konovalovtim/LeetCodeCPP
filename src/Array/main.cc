05a770d58338629343680d4fc99b7b4bde52a56f
Timur <timkonovalov9858@gmail.com>
2023-03-14 20:54:38 +0300

    singleNumber

--- src/Array/main.cc
+++ src/Array/main.cc
@@ -1,12 +1,35 @@
-#include "ContainsDuplicate.cc"
-#include "iostream"
+#include <iostream>
+#include "string"
+#include "../Strings/ReverseInteger.cc"
 
-int main() {
-  Solution *solution = new Solution();
-  std::vector<int> prices = {2, 2, 18, 22, 22};
-  std::cout << solution->containsDuplicate(prices) << std::endl;
-  return 0;
+using namespace std;
+
+int main()
+{
+  int x = 1534236469;
+  int res = 0;
+  std::string str = std::to_string(x);
+  std::string buff{};
+  int flag = 0;
+  size_t i = 0;
+  size_t j = 0;
+  if (x < 0) {
+    buff += '-';
+    j = 1;
+  }
+  for (i = str.length() - 1; i != j - 1; i--) {
+    if (str[i] != '0') {
+      flag = 1;
+    }
+    if (flag == 1) {
+      buff += str[i];
+    }
+  }
+  std::cout << buff << "    ";
+  res = std::stoi(buff);
+   if (res > INT_MAX || res < INT_MIN) {
+     return 0;
+   } else {
+     return res;
+   }
 }
-
-//      profit = *max_element(prices.begin(), prices.end());
-//      profit2 = *min_element(prices.begin(), prices.end());
